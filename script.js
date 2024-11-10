b190c91a4c6eed6f3345b8473b3a9b1bca320884
alnafeel02 <163827591+alnafeel02@users.noreply.github.com>
2024-11-10 12:14:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+
+var sidenav = document.querySelector(".side-nav")
+
+
+function show(){
+    sidenav.style.left ="0"
+}
+function closenav(){
+    sidenav.style.left ="-60%"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
